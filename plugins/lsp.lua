0a6013fb69f3be01b4d7da7727724e9d68db153f
Jared Yang <nociszy@gmail.com>
2024-10-10 22:54:55 -0700

    Update lsp.lua

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -1,6 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- disable signature help in insert mode
+      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
     opts = {
       servers = {
         cssmodules_ls = {},
