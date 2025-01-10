4e39a42e2300a66926205c2263081e6efab65a53
Jared Yang <nociszy@gmail.com>
2025-01-10 09:36:57 -0800

    Update blink.lua

--- plugins/blink.lua
+++ plugins/blink.lua
@@ -1,5 +1,5 @@
 return {
-  -- "saghen/blink.cmp",
+  -- "saghen/blink.cmp", enabled = false,
   -- opts = function(_, opts)
   --   opts.completion.list.selection = { preselect = false, auto_insert = true }
   -- end,
