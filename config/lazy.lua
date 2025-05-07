2d7abf906c92b0fdbdae81888c641a58e509b129
Jared Yang <nociszy@gmail.com>
2025-05-07 09:52:59 -0700

    Update lazy.lua

--- config/lazy.lua
+++ config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     -- import/override with your plugins
     { import = "plugins" },
   },
