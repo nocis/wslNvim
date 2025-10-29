8dbc6ab2b1830df2f286659d6ba2ccffc88bb792
Jared Yang <nociszy@gmail.com>
2025-10-30 05:25:58 +0800

    Update lazy.lua

--- config/lazy.lua
+++ config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.astro" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
