eba8301ef35d79a90fbdf5f685812bcda0393dda
nocis <nocis@tju.edu.cn>
2025-11-22 23:18:26 +0800

    conceal 0 for mdx

--- config/options.lua
+++ config/options.lua
@@ -9,6 +9,7 @@ vim.opt.relativenumber = false
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.laststatus = 2 -- always: show for every window
 vim.opt.completeopt = "menu,menuone,preview,noinsert,noselect"
+vim.opt.conceallevel = 0
 
 vim.o.expandtab = true -- "Use softtabstop spaces instead of tab characters for indentation
 vim.o.shiftwidth = 4 -- "Indent by 4 spaces when using >>, <<, == etc.
