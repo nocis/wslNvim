8057c8da9797807e3edf283831ac416df264915b
nocis <nocis@tju.edu.cn>
2025-12-05 09:25:57 +0800

    add bookmark.lua

---
+++ nvimConfig/lua/plugins/bookmark.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"tomasky/bookmarks.nvim",
+		event = "FileType",
+		keys = {
+			{ "<leader><Tab>", "", desc = "+bookmarks" },
+			{
+				"<leader><Tab><Tab>",
+				function()
+					require("bookmarks").bookmark_toggle()
+				end,
+				desc = "Toggle mark",
+			},
+			{
+				"<leader><Tab>e",
+				function()
+					require("bookmarks").bookmark_ann()
+				end,
+				desc = "Edit mark",
+			},
+			{
+				"<leader><Tab>l",
+				function()
+					require("bookmarks").bookmark_list()
+				end,
+				desc = "List marks",
+			},
+		},
+		opts = {
+			save_file = vim.fn.expand("$HOME/.config/nvim/.bookmarks"),
+			keywords = {
+				["@t"] = " ",
+				["@w"] = " ",
+				["@f"] = "⛏ ",
+				["@n"] = " ",
+			},
+		},
+	},
+}
