5b65243610303145b4dea5370b59a1beb382b617
nocis <nocis@tju.edu.cn>
2026-02-18 04:12:58 +0800

    upadate cmake formatter

--- nvimConfig/lua/plugins/conform.lua
+++ nvimConfig/lua/plugins/conform.lua
@@ -1,35 +1,43 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      log_level = vim.log.levels.DEBUG,
-      formatters_by_ft = {
-        ["javascript"] = { "prettier" },
-        ["javascriptreact"] = { "prettier" },
-        ["typescript"] = { "prettier" },
-        ["typescriptreact"] = { "prettier" },
-        ["vue"] = { "prettier" },
-        ["css"] = { "prettier" },
-        ["scss"] = { "prettier" },
-        ["less"] = { "prettier" },
-        ["html"] = { "prettier" },
-        ["json"] = { "prettier" },
-        ["jsonc"] = { "prettier" },
-        ["yaml"] = { "prettier" },
-        ["markdown"] = { "injected" },
-        ["markdown.mdx"] = { "injected" },
-        ["graphql"] = { "prettier" },
-        ["handlebars"] = { "prettier" },
-        ["cpp"] = { "clang_format" },
-        ["cmake"] = { "cmake_format" },
-        ["python"] = { "ruff", "ruff_organize_imports", "ruff_format" },
-      },
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"gersemi",
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			log_level = vim.log.levels.DEBUG,
+			formatters_by_ft = {
+				["javascript"] = { "prettier" },
+				["javascriptreact"] = { "prettier" },
+				["typescript"] = { "prettier" },
+				["typescriptreact"] = { "prettier" },
+				["vue"] = { "prettier" },
+				["css"] = { "prettier" },
+				["scss"] = { "prettier" },
+				["less"] = { "prettier" },
+				["html"] = { "prettier" },
+				["json"] = { "prettier" },
+				["jsonc"] = { "prettier" },
+				["yaml"] = { "prettier" },
+				["markdown"] = { "injected" },
+				["markdown.mdx"] = { "injected" },
+				["graphql"] = { "prettier" },
+				["handlebars"] = { "prettier" },
+				["cpp"] = { "clang_format" },
+				["cmake"] = { "gersemi" },
+				["python"] = { "ruff", "ruff_organize_imports", "ruff_format" },
+			},
 
-      lang_to_ft = {
-        python = "python",
-      },
-    },
-  },
+			lang_to_ft = {
+				python = "python",
+			},
+		},
+	},
 }
 
 -- need to format injected/embedded code block in markdown manully
