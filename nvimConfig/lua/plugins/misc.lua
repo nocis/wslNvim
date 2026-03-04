a801fe43bb515b4cf939fa30ecbe54db1b87aa5f
nocis <nocis@tju.edu.cn>
2026-03-04 19:01:02 +0800

    update leetcode config

--- nvimConfig/lua/plugins/misc.lua
+++ nvimConfig/lua/plugins/misc.lua
@@ -1,9 +1,28 @@
 local leet_arg = "leetcode.nvim"
 
 return {
-  {
-    "kawre/leetcode.nvim",
-    lazy = leet_arg ~= vim.fn.argv(0, -1),
-    opts = { arg = leet_arg },
-}
+	{
+		"kawre/leetcode.nvim",
+		lazy = leet_arg ~= vim.fn.argv(0, -1),
+		opts = {
+			lang = "python3",
+			arg = leet_arg,
+			injector = { ---@type table<lc.lang, lc.inject>
+				["python3"] = {
+					imports = function(default_imports)
+						-- vim.list_extend(default_imports, { "from .leetcode import *" })
+						return { "from .leetcode import *" }
+					end,
+					after = { "def test():", "    print('test')" },
+				},
+				["cpp"] = {
+					imports = function()
+						-- return a different list to omit default imports
+						return { "#include <bits/stdc++.h>", "using namespace std;" }
+					end,
+					after = "int main() {}",
+				},
+			},
+		},
+	},
 }
